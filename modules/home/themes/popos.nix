de774ba53d5fdbaf54b3796e1ee56a6a9d87ecfa
Matthew English <matt.english4@gmail.com>
2026-04-06 17:33:18 -0230

    Add missing PopOS config pieces

--- modules/home/themes/popos.nix
+++ modules/home/themes/popos.nix
@@ -109,9 +109,42 @@ in
     # XFCE panel config (unchanged logic)
     ############################
     xfconf.settings = {
+      xfce4-desktop {
+        "backdrop/single-workspace-mode" = true;
+        "backdrop/single-image-mode" = true;
+        "backdrop/screen0/monitor0/workspace0/last-image" = "${config.home.homeDirectory}/pictures/current-wallpaper.jpg";
+      }
       xfce4-panel = {
         "panels" = [ 1 2 ];
-        # ... keep your existing panel/dock settings here ...
+
+        # Top panel
+        "panels/panel-1/position"      = "p=6;x=0;y=0";
+        "panels/panel-1/size"          = 28;
+        "panels/panel-1/length"        = 100;
+        "panels/panel-1/length-adjust" = true;
+        "panels/panel-1/mode"          = 0;
+        "panels/panel-1/position-locked"  = true;
+        "panels/panel-1/plugin-ids" = [ 1 2 3 4 5 6 7 ];
+
+        "plugins/plugin-1" = "whiskermenu";
+        "plugins/plugin-3" = "clock";
+        "plugins/plugin-3/digital-format" = "%b %-e %i:%M %p";
+        "plugins/plugin-5" = "systray";
+        "plugins/plugin-6" = "power-manager-plugin";
+        "plugins/plugin-7" = "launcher";
+        "plugins/plugin-7/items" = [ "pop-quick-menu.desktop" ];
+
+        # Bottom panel (dock)
+        "panels/panel-2/position"      = "p=10;x=0;y=0";
+        "panels/panel-2/size"          = 46;
+        "panels/panel-2/length"        = 100;
+        "panels/panel-2/length-adjust" = true;
+        "panels/panel-2/mode"          = 0;
+        "panels/panel-2/position-locked"  = true;
+        "panels/panel-2/autohide-behavior" = 0;
+
+        "panels/panel-2/plugin-ids" = [ 20 dockConfig.id 22 ];
+        "plugins/plugin-${toString dockConfig.id}" = "docklike";
       }
       // mkSeparator 2
       // mkSeparator 4
@@ -119,7 +152,62 @@ in
       // mkSeparator dockConfig.id;
     };
 
-    # Docklike plugin config file, quick menu, wallpapers, etc.
-    # (copy over from your existing popos.nix as-is)
+    
+    ############################
+    # Docklike plugin config
+    ############################
+    home.file.".config/xfce4/panel/docklike-${toString dockConfig.id}.rc".source =
+      pkgs.writeText "docklike-${toString dockConfig.id}.rc" ''
+        [user]
+        indicatorStyle=4
+        inactiveIndicatorStyle=4
+        indicatorOrientation=1
+        indicatorColor=${dockConfig.activeIndicatorColor}
+        inactiveColor=${dockConfig.inactiveIndicatorColor}
+        pinned=firefox;
+        onlyDisplayVisible=true
+        forceIconSize=true
+        iconSize=32
+      '';
+
+    ############################
+    # Pop-style quick menu
+    ############################
+    home.file.".local/bin/pop-quick-menu" = {
+      text = ''
+        #!/usr/bin/env bash
+        choice=$(printf '%s\n' \
+          "  Sound" \
+          "  Bluetooth" \
+          "  Night Light" \
+          "  Settings" \
+          "⏻  Power…" \
+          | rofi -dmenu -p "Quick Settings")
+
+        case "$choice" in
+          "  Sound")
+            pavucontrol >/dev/null 2>&1 & ;;
+          "  Bluetooth")
+            rofi-bluetooth >/dev/null 2>&1 & ;;
+          "  Night Light")
+            pkill -x redshift || redshift -O 4500K >/dev/null 2>&1 & ;;
+          "  Settings")
+            xfce4-settings-manager >/dev/null 2>&1 & ;;
+          "⏻  Power…")
+            rofi -show power-menu -modi power-menu:rofi-power-menu ;;
+        esac
+      '';
+      executable = true;
+    };
+
+    home.file.".local/share/applications/pop-quick-menu.desktop".text = ''
+      [Desktop Entry]
+      Type=Application
+      Name=Quick Settings
+      Comment=PopOS-style quick settings menu
+      Exec=/home/matthew/.local/bin/pop-quick-menu
+      Icon=preferences-system
+      Terminal=false
+    '';
   };
 }
