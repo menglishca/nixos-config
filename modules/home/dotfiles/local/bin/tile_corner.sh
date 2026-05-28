524ffa8c51da413e88710a870b1adbde273bf59c
Matthew English <matt.english4@gmail.com>
2026-05-28 17:26:43 -0230

    Fix tiling script to account for bottom panel

--- modules/home/dotfiles/local/bin/tile_corner.sh
+++ modules/home/dotfiles/local/bin/tile_corner.sh
@@ -1,39 +1,41 @@
-#!/bin/sh
-# Corner tiling script
+#!/usr/bin/env bash
 CORNER="$1"
 
 # Get screen dimensions
 SCREEN=$(wmctrl -d | grep '*' | awk '{print $4}')
-WIDTH=$(echo "$SCREEN" | cut -d'x' -f1)
-HEIGHT=$(echo "$SCREEN" | cut -d'x' -f2)
+SCREEN_WIDTH=$(echo "$SCREEN" | cut -d'x' -f1)
+SCREEN_HEIGHT=$(echo "$SCREEN" | cut -d'x' -f2)
+BOTTOM_PANEL_HEIGHT=$(wmctrl -lG | awk -v height="${SCREEN_HEIGHT}" '/xfce4-panel/ && ($4+$6) == height {print $6}')
+BOTTOM_PANEL_HEIGHT=${BOTTOM_PANEL_HEIGHT:-0}
+AVAILABLE_HEIGHT=$((SCREEN_HEIGHT - BOTTOM_PANEL_HEIGHT))
 
 # Quarter-screen dimensions
-QW=$((WIDTH / 2))
-QH=$((HEIGHT / 2))
+HALF_SCREEN_WIDTH=$((SCREEN_WIDTH / 2))
+HALF_SCREEN_HEIGHT=$((AVAILABLE_HEIGHT / 2))
 
 case "$CORNER" in
     top-left)
-        wmctrl -r :ACTIVE: -e 0,0,0,$QW,$QH
+        wmctrl -r :ACTIVE: -e 0,0,0,$HALF_SCREEN_WIDTH,$HALF_SCREEN_HEIGHT
         ;;
     top-right)
-        wmctrl -r :ACTIVE: -e 0,$((WIDTH - QW)),0,$QW,$QH
+        wmctrl -r :ACTIVE: -e 0,$((SCREEN_WIDTH - HALF_SCREEN_WIDTH)),0,$HALF_SCREEN_WIDTH,$HALF_SCREEN_HEIGHT
         ;;
     bottom-left)
-        wmctrl -r :ACTIVE: -e 0,0,$((HEIGHT - QH)),$QW,$QH
+        wmctrl -r :ACTIVE: -e 0,0,$((AVAILABLE_HEIGHT - HALF_SCREEN_HEIGHT)),$HALF_SCREEN_WIDTH,$HALF_SCREEN_HEIGHT
     ;;
     bottom-right)
-        wmctrl -r :ACTIVE: -e 0,$((WIDTH - QW)),$((HEIGHT - QH)),$QW,$QH
+        wmctrl -r :ACTIVE: -e 0,$((SCREEN_WIDTH - HALF_SCREEN_WIDTH)),$((AVAILABLE_HEIGHT - HALF_SCREEN_HEIGHT)),$HALF_SCREEN_WIDTH,$HALF_SCREEN_HEIGHT
         ;;
     top)
-        wmctrl -r :ACTIVE: -e 0,0,0,$WIDTH,$QH
+        wmctrl -r :ACTIVE: -e 0,0,0,$SCREEN_WIDTH,$HALF_SCREEN_HEIGHT
         ;;
     bottom)
-        wmctrl -r :ACTIVE: -e 0,0,$((HEIGHT - QH)),$WIDTH,$QH
+        wmctrl -r :ACTIVE: -e 0,0,$((AVAILABLE_HEIGHT - HALF_SCREEN_HEIGHT)),$SCREEN_WIDTH,$HALF_SCREEN_HEIGHT
         ;;
     left)
-        wmctrl -r :ACTIVE: -e 0,0,0,$QW,$HEIGHT
+        wmctrl -r :ACTIVE: -e 0,0,0,$HALF_SCREEN_WIDTH,$AVAILABLE_HEIGHT
         ;;
     right)
-        wmctrl -r :ACTIVE: -e 0,$((WIDTH - QW)),0,$QW,$HEIGHT
+        wmctrl -r :ACTIVE: -e 0,$((SCREEN_WIDTH - HALF_SCREEN_WIDTH)),0,$HALF_SCREEN_WIDTH,$AVAILABLE_HEIGHT
         ;;
 esac
