6189e2fe21e2bacbfd9210f2671ac56809ac6f52
Matthew English <matt.english4@gmail.com>
2026-05-28 12:56:25 -0230

    Try again

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -4,13 +4,22 @@
 {
   xdg = {
     enable = true;
+    desktopEntries = {
+      alacritty = {
+        name = "Alacritty";
+        exec = "${pkgs.alacritty}/bin/alacritty";
+        icon = "terminal";
+        categories = [ "System" "TerminalEmulator" ];
+        mimeType = [ "text/plain" ];
+      };
+    };
 
     userDirs = let
       lower = path: "${config.home.homeDirectory}/${path}";
     in {
       enable = true;
       createDirectories = true;
-      userDirs.setSessionVariables = true;
+      setSessionVariables = true;
 
       desktop     = lower "desktop";
       documents   = lower "documents";
@@ -20,15 +29,6 @@
       publicShare = lower "public";
       templates   = lower "templates";
       videos      = lower "videos";
-      desktopEntries = {
-        alacritty = {
-          name = "Alacritty";
-          exec = "${pkgs.alacritty}/bin/alacritty";
-          icon = "terminal";
-          categories = [ "System" "TerminalEmulator" ];
-          mimeType = [ "text/plain" ];
-        };
-      };
     };
   };
 }
