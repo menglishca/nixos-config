120ce217f70e60b7100c08551c1d53552b1e36d9
Matthew English <matt.english4@gmail.com>
2026-05-28 16:48:36 -0230

    Fix dotfiles and spacing

--- modules/home/base.nix
+++ modules/home/base.nix
@@ -2,55 +2,55 @@
 { config, lib, pkgs, ... }:
 
 let
-  populateDotfiles = { dotfilesDir, prefixDot ? true }:
-    let
-      recursivelyGetFiles = relativePath:
+    populateDotfiles = { dotfilesDir, prefixDot ? true }:
         let
-          dotfilesSourcePath = if relativePath == "" then dotfilesDir else dotfilesDir + "/${relativePath}";
+            recursivelyGetFiles = relativePath:
+                let
+                    dotfilesSourcePath = if relativePath == "" then dotfilesDir else dotfilesDir + "/${relativePath}";
 
-          entries =
-            if builtins.pathExists dotfilesSourcePath
-            then builtins.readDir dotfilesSourcePath
-            else {};
+                    entries =
+                        if builtins.pathExists dotfilesSourcePath
+                        then builtins.readDir dotfilesSourcePath
+                        else {};
+                in
+                    lib.concatMap
+                        (fileName:
+                            let
+                                filetype = entries.${fileName};
+                                childRelativePath = if relativePath == "" then fileName else "${relativePath}/${fileName}";
+                            in
+                                if filetype == "directory"
+                                then recursivelyGetFiles childRelativePath
+                                else
+                                    let
+                                        pathParts  = lib.splitString "/" childRelativePath;
+                                        top        = lib.head pathParts;
+                                        rest       = lib.tail pathParts;
+                                        topWithDot = if prefixDot then ".${top}" else top;
+                                        homePath   = lib.concatStringsSep "/" ([ topWithDot ] ++ rest);
+                                    in
+                                        [
+                                            {
+                                                name  = homePath;
+                                                value = { source = dotfilesDir + "/${childRelativePath}"; };
+                                            }
+                                        ]
+                        )
+                        (lib.attrNames entries);
         in
-          lib.concatMap
-            (fileName:
-              let
-                filetype = entries.${fileName};
-                childRelativePath = if relativePath == "" then fileName else "${relativePath}/${fileName}";
-              in
-                if filetype == "directory"
-                then recursivelyGetFiles childRelativePath
-                else
-                  let
-                    pathParts  = lib.splitString "/" childRelativePath;
-                    top        = lib.head pathParts;
-                    rest       = lib.tail pathParts;
-                    topWithDot = if prefixDot then ".${top}" else top;
-                    homePath   = lib.concatStringsSep "/" ([ topWithDot ] ++ rest);
-                  in
-                    [
-                      {
-                        name  = homePath;
-                        value = { source = dotfilesDir + "/${childRelativePath}"; };
-                      }
-                    ]
-            )
-            (lib.attrNames entries);
+            lib.listToAttrs (recursivelyGetFiles "");
+    mkDotfiles =  { path, name, prefixDot ? true }:
+        if !builtins.pathExists path then
+            {}
+        else
+            let
+                dotfilesDir = builtins.path { inherit path name; };
+            in
+                populateDotfiles { inherit dotfilesDir prefixDot; };
     in
-      lib.listToAttrs (recursivelyGetFiles "");
-  mkDotfiles =  { path, name, prefixDot ? true }:
-    if !builtins.pathExists path then
-      {}
-    else
-      let
-        dotfilesDir = builtins.path { inherit path name; };
-      in
-        populateDotfiles { inherit dotfilesDir prefixDot; };
-in
-{
-  home.stateVersion = "25.11";
-  programs.home-manager.enable = true;
-  gtk.gtk4.theme = config.gtk.theme;
-  _module.args.mkDotfiles = mkDotfiles;
-}
+        {
+            home.stateVersion = "25.11";
+            programs.home-manager.enable = true;
+            gtk.gtk4.theme = config.gtk.theme;
+            _module.args.mkDotfiles = mkDotfiles;
+        }
