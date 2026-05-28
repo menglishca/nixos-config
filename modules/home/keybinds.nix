b79c0c6eab533b0669d1aa5139260353acbf8143
Matthew English <matt.english4@gmail.com>
2026-05-28 18:02:05 -0230

    Attempt to fix keybinds for snapping

--- modules/home/keybinds.nix
+++ modules/home/keybinds.nix
@@ -34,10 +34,10 @@
                 # App launcher (Rofi)
                 "commands/custom/<Control>space" = "${pkgs.rofi}/bin/rofi -show drun";                
                 # Window snapping (halves)
-                "commands/custom/<Super>Left" = "${config.home.homeDirectory}/.local/bin/tile_corner.sh left";
-                "commands/custom/<Super>Right" = "${config.home.homeDirectory}/.local/bin/tile_corner.sh right";
-                "commands/custom/<Super>Up" = "${config.home.homeDirectory}/.local/bin/tile_corner.sh top";
-                "commands/custom/<Super>Down" = "${config.home.homeDirectory}/.local/bin/tile_corner.sh bottom";
+                "commands/custom/<Super>Left" = "tile_left_key";
+                "commands/custom/<Super>Right" = "tile_right_key";
+                "commands/custom/<Super>Up" = "tile_up_key";
+                "commands/custom/<Super>Down" = "tile_down_key";
                 "commands/custom/<Super>KP7" = "${config.home.homeDirectory}/.local/bin/tile_corner.sh top-left";
                 "commands/custom/<Super>KP9" = "${config.home.homeDirectory}/.local/bin/tile_corner.sh top-right";
                 "commands/custom/<Super>KP1" = "${config.home.homeDirectory}/.local/bin/tile_corner.sh bottom-left";
